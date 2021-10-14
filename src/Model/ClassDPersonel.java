f283cdd230d110a49c9d11aa68003ce7dc109213
WhaleAid <khalqallahwalid@gmail.com>
2021-10-14 15:50:49 +0200

    first commit

---
+++ src/Model/ClassDPersonel.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class ClassDPersonel {
+    private int number;
+    private String name;
+    private int age;
+    private String Description;
+    private  String file;
+
+    public ClassDPersonel(int number, String name, int age, String description, String file) {
+        this.number = number;
+        this.name = name;
+        this.age = age;
+        Description = description;
+        this.file = file;
+    }
+}
---
+++ src/Model/scp.java
@@ -0,0 +1,5 @@
+package Model;
+
+public class scp {
+    private int 
+}
